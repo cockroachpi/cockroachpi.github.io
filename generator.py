80d43869d80780f32a28fd974e4887ef87e95a95
cockroachpi <122821509+cockroachpi@users.noreply.github.com>
2023-01-18 14:37:49 +0000

    .

--- generator.py
+++ generator.py
@@ -12,7 +12,7 @@ a = """<!DOCTYPE html>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
-    <style>* {background-color: "#420420"; color: "#696969"; font-family: "Montserrat";}</style>
+    <style>* {background-color: #242420; color: "#696969"; font-family: "Montserrat";}</style>
 </head>
 <body>"""
 
@@ -20,11 +20,11 @@ z = """</body>
 </html>"""
 
 content_is_said_too_much = ""
-for note in os.listdir(NOTES_PATH):
-    with open(NOTES_PATH + note, "r") as note_file:
+for note in os.listdir (NOTES_PATH):
+    with open (NOTES_PATH + note, "r") as note_file:
         content_is_said_too_much += markdown.markdown(note_file.read())
 
-with open("index.html", "w") as page:
+with open ("index.html", "w") as page:
     page.write(a + content_is_said_too_much + z)
 
 print ("gerenrantor is ending")
