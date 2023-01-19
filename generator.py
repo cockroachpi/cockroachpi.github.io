b232970f40d20eb8aac498cbe1840d66e26061fd
cockroachpi <122821509+cockroachpi@users.noreply.github.com>
2023-01-19 16:12:29 +0000

    trochę brzydkiego kodu

--- generator.py
+++ generator.py
@@ -12,17 +12,33 @@ a = """<!DOCTYPE html>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
-    <style>* {background-color: #242420; color: #696969; font-family: "Montserrat";}</style>
+    <style>
+        body {
+        background-color: #242420; 
+        color: #696969; 
+        font-family: "Montserrat";
+        margin-left: 30px;
+        margin-right: 50px;
+        margin-top: 3px;
+        }
+    </style>
 </head>
-<body>"""
+<body>
+    <h1>bunt maszyn w końcu nadejdzie</h1>"""
 
 z = """</body>
 </html>"""
 
+def compile_post(path):
+    with open(path, 'r') as file:
+        return "<div>" + "".join(markdown.markdown(file.read())) + "</div>"
+
 content_is_said_too_much = ""
-for note in os.listdir (NOTES_PATH):
-    with open (NOTES_PATH + note, "r") as note_file:
-        content_is_said_too_much += markdown.markdown(note_file.read())
+posty = os.listdir(NOTES_PATH)
+posty.reverse()
+print(posty)
+for note in posty:
+    content_is_said_too_much += compile_post(NOTES_PATH+note)
 
 with open ("index.html", "w") as page:
     page.write(a + content_is_said_too_much + z)
